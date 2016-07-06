1e9faf94c5d21738844f639be429538edacb184d
przemek <przemek>
2016-07-06 15:52:37 +0100

    Finished 2 side gauge

--- Bootstrap/Scripts/d3.js
+++ Bootstrap/Scripts/d3.js
@@ -1,65 +1,132 @@
 var Gauge;
-(function (Gauge) {
-    var margin = { top: 30, right: 20, bottom: 30, left: 50 };
-    var containerWidth = 600;
-    var containerHeight = 150;
-    var height = containerHeight - margin.top - margin.bottom;
-    //Maximum value for this gauge is defined by
-    //containerWidth - margin.left - margin.right;
-    var width = containerWidth - margin.left - margin.right;
-    var dataValue = [0];
-    //Changing range values changes scale start and end point
-    var xAxisScale = d3.scale.linear().range([0, width]).domain([0, 100]);
-    //Make container which holds scale and moving bar
-    var svgContainer = d3.select("#content")
-        .append("svg")
-        .attr("width", containerWidth)
-        .attr("height", containerHeight)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    //.attr("style", "outline: thin solid blue;");
-    //create rectangle based on passed value dataValue
-    svgContainer.selectAll("rect")
-        .data(dataValue)
-        .enter()
-        .append("rect")
-        .attr({
-        x: 0,
-        y: 0,
-        width: function (d, i) { return d; },
-        height: height,
-        fill: function (d, i) { return "rgb(" + 0 + ", 10, 100)"; }
-    });
-    //Axis
-    var xAxis = d3.svg.axis().scale(xAxisScale)
-        .orient("bottom").ticks(10);
-    svgContainer.append("g") // Add the X Axis
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-    //Sets up the connection to Hub
-    var d3DataHub = $.connection.d3DataHub;
-    $.connection.hub.logging = true;
-    $.connection.hub.start()
-        .done(function () {
-        d3DataHub.server.strartUpdatingValues();
-    });
-    //Deals with info incoming from server
-    d3DataHub.client.gaugeState = function (message) {
-        changeData(message);
-    };
-    var updateDelay = 1000;
-    function changeData(data) {
-        dataValue[0] = data;
-        svgContainer.selectAll("rect")
-            .data(dataValue)
-            .transition()
-            .duration(1000)
-            .ease("linear") //default, others are bounce, elastic, circle
-            .attr({
-            width: function (d, i) { return d; },
-        });
-    }
+(function (Gauge_1) {
+    var Direction;
+    (function (Direction) {
+        Direction[Direction["Left"] = 0] = "Left";
+        Direction[Direction["Right"] = 1] = "Right";
+    })(Direction || (Direction = {}));
+    var Margin = (function () {
+        function Margin(left, right, top, bottom) {
+            if (left === void 0) { left = 50; }
+            if (right === void 0) { right = 20; }
+            if (top === void 0) { top = 30; }
+            if (bottom === void 0) { bottom = 30; }
+            this.left = left;
+            this.right = right;
+            this.top = top;
+            this.bottom = bottom;
+        }
+        return Margin;
+    }());
+    var Gauge = (function () {
+        function Gauge(containerWidth, containerHeight, margin, orientation) {
+            this.multiplierConstant = 5.3;
+            this.dataValue = [60];
+            this.containerWidth = containerWidth;
+            this.containerHeight = containerHeight;
+            this.margin = margin;
+            this.height = containerHeight - this.margin.top - this.margin.bottom;
+            //Maximum value for this gauge is defined by
+            //containerWidth - margin.left - margin.right;
+            this.width = containerWidth - margin.left - margin.right;
+            this.multiplierConstant = this.width / 100;
+            this.svgContainer = this.createContainer();
+            this.caclculateProperties(orientation);
+        }
+        Gauge.prototype.caclculateProperties = function (orientation) {
+            if (orientation == 0) {
+                this.leftScaleValue = this.width;
+                this.rightScaleValue = 0;
+                this.direction = 1;
+                return;
+            }
+            //left to right
+            this.leftScaleValue = 0;
+            this.rightScaleValue = this.width;
+            this.direction = 0;
+        };
+        //Make container which holds scale and moving bar
+        Gauge.prototype.createContainer = function () {
+            var container = d3.select("#content")
+                .append("svg")
+                .attr("width", this.containerWidth)
+                .attr("height", this.containerHeight)
+                .append("g")
+                .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+            return container;
+        };
+        ;
+        Gauge.prototype.createRectangle = function () {
+            var _this = this;
+            //create rectangle based on passed value dataValue
+            this.svgContainer.selectAll("rect")
+                .data(this.dataValue)
+                .enter()
+                .append("rect")
+                .attr({
+                y: 0,
+                //direction has to be 0 for left to right
+                x: function (d, i) {
+                    console.log(_this.width - d * _this.multiplierConstant);
+                    return (_this.width - d * _this.multiplierConstant) * _this.direction;
+                },
+                width: function (d, i) { return d * _this.multiplierConstant; },
+                height: this.height,
+                fill: function (d, i) { return "rgb(" + 0 + ", 10, 100)"; }
+            });
+            return this;
+        };
+        Gauge.prototype.createAxis = function () {
+            //Changing range values changes scale start and end point
+            var xAxisScale = d3.scale.linear().range([this.leftScaleValue, this.rightScaleValue]).domain([0, 100]);
+            //Axis
+            var xAxis = d3.svg.axis().scale(xAxisScale)
+                .orient("bottom").ticks(10);
+            this.svgContainer.append("g") // Add the X Axis
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + this.height + ")")
+                .call(xAxis);
+            return this;
+        };
+        Gauge.prototype.setUpD3 = function () {
+            var _this = this;
+            //Sets up the connection to Hub
+            var d3DataHub = $.connection.d3DataHub;
+            $.connection.hub.logging = true;
+            $.connection.hub.start()
+                .done(function () {
+                d3DataHub.server.strartUpdatingValues();
+            });
+            //Deals with info incoming from server
+            d3DataHub.client.gaugeState = function (message) {
+                _this.changeData(_this.dataValue, message);
+            };
+        };
+        Gauge.prototype.changeData = function (dataValue, serverData) {
+            var _this = this;
+            this.dataValue[0] = serverData;
+            this.svgContainer.selectAll("rect")
+                .data(this.dataValue)
+                .transition()
+                .duration(1000)
+                .ease("linear") //default, others are bounce, elastic, circle
+                .attr({
+                x: function (d, i) {
+                    console.log(_this.width - d * _this.multiplierConstant);
+                    return (_this.width - d * _this.multiplierConstant) * _this.direction;
+                },
+                width: function (d, i) { return d * _this.multiplierConstant; },
+            });
+        };
+        return Gauge;
+    }());
+    Gauge_1.Gauge = Gauge;
+    Gauge_1.gauge = new Gauge(600, 150, new Margin(50, 20, 30, 30), 1);
+    Gauge_1.gauge.createRectangle().createAxis();
+    Gauge_1.gauge.setUpD3();
+    Gauge_1.gauge1 = new Gauge(600, 150, new Margin(50, 20, 30, 30), 0);
+    Gauge_1.gauge1.createRectangle().createAxis();
+    Gauge_1.gauge1.setUpD3();
 })(Gauge || (Gauge = {}));
 /*
 svg.selectAll("text")
@@ -103,4 +170,3 @@ class Greeter {
     greeter.start();
 };
 */ 
-//# sourceMappingURL=d3.js.map
--- Bootstrap/Scripts/d3.ts
+++ Bootstrap/Scripts/d3.ts
@@ -1,77 +1,164 @@
 ﻿declare var $;
 
 module Gauge {
-    const margin = { top: 30, right: 20, bottom: 30, left: 50 };
-    const containerWidth: number = 600;
-    const containerHeight: number = 150;
-    const height: number = containerHeight - margin.top - margin.bottom;
-    //Maximum value for this gauge is defined by
-    //containerWidth - margin.left - margin.right;
-    const width: number = containerWidth - margin.left - margin.right;
-    var dataValue: [number] = [0];
 
-    //Changing range values changes scale start and end point
-    var xAxisScale = d3.scale.linear().range([0, width]).domain([0, 100]);
-
-    //Make container which holds scale and moving bar
-    var svgContainer: any = d3.select("#content")
-        .append("svg")
-        .attr("width", containerWidth)
-        .attr("height", containerHeight)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    //.attr("style", "outline: thin solid blue;");
-
-    //create rectangle based on passed value dataValue
-    svgContainer.selectAll("rect")
-        .data(dataValue)
-        .enter()
-        .append("rect")
-        .attr({
-            x: 0,
-            y: 0,
-            width: function (d, i) { return d; },
-            height: height,
-            fill: function (d, i) { return "rgb(" + 0 + ", 10, 100)" }
-        });
-
-    //Axis
-    var xAxis = d3.svg.axis().scale(xAxisScale)
-        .orient("bottom").ticks(10);
-
-    svgContainer.append("g")         // Add the X Axis
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    //Sets up the connection to Hub
-    var d3DataHub = $.connection.d3DataHub;
-    $.connection.hub.logging = true;
-    $.connection.hub.start()
-        .done(function () {
-            d3DataHub.server.strartUpdatingValues();
-        });
-
-    //Deals with info incoming from server
-    d3DataHub.client.gaugeState = function (message): void {
-        changeData(message);
+    enum Direction {
+        Left = 0,
+        Right
     }
 
-    var updateDelay = 1000;
-    function changeData(data): void {
-        dataValue[0] = data;
-        svgContainer.selectAll("rect")
-            .data(dataValue)
-            .transition()
-            .duration(1000)
-            .ease("linear")//default, others are bounce, elastic, circle
-            //Change the actual value
-            .attr({
-                width: function (d, i) { return d; },
-            });
+
+    class Margin {
+        public top: number;
+        public right: number;
+        public left: number;
+        public bottom: number;
+        public constructor(left = 50, right = 20, top = 30, bottom = 30) {
+            this.left = left;
+            this.right = right;
+            this.top = top;
+            this.bottom = bottom;
+        }
     }
 
+    export class Gauge {
+        private height: number;
+        private width: number;
+        private margin: Margin;
+        private containerWidth: number;
+        private containerHeight: number;
+        private multiplierConstant: number = 5.3;
+        private dataValue = [60];
+        private svgContainer: any;
+
+        private leftScaleValue;
+        private rightScaleValue;
+        private xPosition;
+        private direction;
+
+        private caclculateProperties(orientation) {
+            if (orientation == 0) {
+                this.leftScaleValue = this.width;
+                this.rightScaleValue = 0;
+                this.direction = 1;
+                return;
+            }
+            //left to right
+            this.leftScaleValue = 0;
+            this.rightScaleValue = this.width;
+            this.direction = 0;
+        }
+
+        public constructor(containerWidth, containerHeight, margin: Margin, orientation: Direction) {
+            this.containerWidth = containerWidth;
+            this.containerHeight = containerHeight;
+            this.margin = margin;
+            this.height = containerHeight - this.margin.top - this.margin.bottom;
+            //Maximum value for this gauge is defined by
+            //containerWidth - margin.left - margin.right;
+            this.width = containerWidth - margin.left - margin.right;
+            this.multiplierConstant = this.width / 100;
+            this.svgContainer = this.createContainer();
+            this.caclculateProperties(orientation);
+
+        }
+
+        //Make container which holds scale and moving bar
+        private createContainer():any {
+            var container = d3.select("#content")
+                .append("svg")
+                .attr("width", this.containerWidth)
+                .attr("height", this.containerHeight)
+                .append("g")
+                .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+            return container;
+        };
+
+        public createRectangle() {
+            //create rectangle based on passed value dataValue
+            this.svgContainer.selectAll("rect")
+                .data(this.dataValue)
+                .enter()
+                .append("rect")
+                .attr({
+                    y: 0,
+                    //direction has to be 0 for left to right
+                    x: (d, i) => {
+                        console.log(this.width - d * this.multiplierConstant);
+                        return (this.width - d * this.multiplierConstant) * this.direction
+                    },
+                    width: (d, i)=> { return d * this.multiplierConstant},
+                    height: this.height,
+                    fill: function (d, i) { return "rgb(" + 0 + ", 10, 100)" }
+                });
+            return this;
+        }
+
+        public createAxis() {
+            //Changing range values changes scale start and end point
+            var xAxisScale = d3.scale.linear().range([this.leftScaleValue, this.rightScaleValue]).domain([0, 100]);
+
+            //Axis
+            var xAxis = d3.svg.axis().scale(xAxisScale)
+                .orient("bottom").ticks(10);
+
+            this.svgContainer.append("g")         // Add the X Axis
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + this.height + ")")
+                .call(xAxis);
+            return this;
+        }
+
+
+        public setUpD3() {
+            //Sets up the connection to Hub
+            var d3DataHub = $.connection.d3DataHub;
+            $.connection.hub.logging = true;
+            $.connection.hub.start()
+                .done(function () {
+                    d3DataHub.server.strartUpdatingValues();
+                });
+
+            //Deals with info incoming from server
+            d3DataHub.client.gaugeState = (message): void => {
+                this.changeData(this.dataValue, message);
+            }
+        }
+        
+        changeData(dataValue, serverData) {
+            this.dataValue[0] = serverData;
+            this.svgContainer.selectAll("rect")
+                .data(this.dataValue)
+                .transition()
+                .duration(1000)
+                .ease("linear")//default, others are bounce, elastic, circle
+                //Change the actual value
+                .attr({
+                    x: (d, i) => {
+                        console.log(this.width - d * this.multiplierConstant);
+                        return (this.width - d * this.multiplierConstant) * this.direction
+                    },
+                    width: (d, i) => { return d * this.multiplierConstant },
+                });
+        }
+    }
+
+
+
+    export var gauge = new Gauge(600, 150, new Margin(50, 20, 30, 30), 1);
+    gauge.createRectangle().createAxis()
+    gauge.setUpD3();
+
+   export var gauge1 = new Gauge(600, 150, new Margin(50, 20, 30, 30), 0);
+   gauge1.createRectangle().createAxis()
+   gauge1.setUpD3();
 }
+
+
+
+
+
+
 
 
 
